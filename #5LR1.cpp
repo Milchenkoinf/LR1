2ed826b4347327165c42dd6e44aa53652f547c0b
Ivan <70635146+themilchenko@users.noreply.github.com>
2020-11-19 21:08:53 +0300

    Update #5LR1.cpp

--- #5LR1.cpp
+++ #5LR1.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <iomanip>
+
 using namespace std;
-int main ()
- {
-    unsigned short int f, e, d, c;
+
+int main()
+{
+    unsigned short int f;
+    unsigned short int e;
+    unsigned short int d;
+    unsigned short int c;
     cout << "Hello, enter values of f, e, d, c respectively" << endl;
     cout << "write f: ";
     cin >> f;
@@ -14,6 +19,6 @@ int main ()
     cout << "write c: ";
     cin >> c;
     unsigned short int x = ~f | e & ~(c ^ d);
-    cout << "!f V e ^ !(c+d) <=> " << showbase << hex << setw(2) << x;
+    cout << "!f V e ^ !(c+d) <=> " << showbase << hex << setw(8) << setfill('0') << x;
     return 0;
 }
